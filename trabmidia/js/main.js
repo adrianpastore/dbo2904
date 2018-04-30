3c2269a06c0d69f3ba85fa0c1d9a28fdb738474e
Adrian Pastore <adrianqpastore@gmail.com>
2018-04-29 23:25:04 -0300

    Add files via upload

--- trabmidia/js/main.js
+++ trabmidia/js/main.js
@@ -1,13 +1,27 @@
 let Assistirvideo = function() {
-  let URL = window.URL || window.webkitURL
-
-  let playSelectedFile = function (event) {
-    let file = this.files[0]
-    let type = file.type
-    let videoNode = document.querySelector('video')
-    let fileURL = URL.createObjectURL(file)
-    videoNode.src = fileURL
+  let URL = window.URL || window.webkitURL;
+  let selecionararquivo = function (e) {
+    let file = this.files[0];
+    let type = file.type;
+    let videoNode = document.querySelector('video');
+    let fileURL = URL.createObjectURL(file);
+    videoNode.src = fileURL;
   }
-  let inputNode = document.querySelector('input')
-  inputNode.addEventListener('change', playSelectedFile, false)
-}()
+  let inputNode = document.querySelector('input');
+  inputNode.addEventListener('change', selecionararquivo, false);
+}();
+
+function NomeArquivo (fileInput) {
+    fileInput.prevent
+    let arqv = fileInput.files;
+    let h2 = document.createElement("h2");
+    for (let i = 0; i < arqv.length; i++) {
+        let htxt = "O nome do Arquivo é: "+(arqv[i].name);
+        let htxtcerto = htxt.replace('.mp4','');;
+        console.log(htxtcerto);
+        let txt = document.createTextNode(htxtcerto);
+        h2.appendChild(txt);
+    }
+    document.body.appendChild(h2);
+}
+
